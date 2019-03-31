be3e83a6ae291c4213af014617cbfbcc5bb54ea3
Raymundo Vasquez Ruiz <raymundo.vr@protonmail.com>
2019-03-31 12:42:55 +0200

    Removed the comparisions with SULFURA, given that it can't be sold (so no changes needed) now is being dismissed immediately

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,6 +15,24 @@ class Shop {
   }
 
   /**
+   * Determines whether an item can be sold
+   * @param item
+   * @returns {boolean}
+   */
+  canBeSold(item) {
+    return item.name !== this.SULFURAS;
+  }
+
+  /**
+   * Determines is the quality can still be updated
+   * @param item
+   * @returns {boolean}
+   */
+  canQualityBeUpdated(item) {
+    return item.quality >= 0 && item.quality <= 50;
+  }
+
+  /**
    * Iterates on the current items and, for each of them,
    * performs an update on its quality
    * iff the conditions, specific for the item type, are met.
@@ -22,46 +40,44 @@ class Shop {
    */
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name !== this.BRIE && item.name !== this.BACKSTAGE_PASSES) {
-        if (item.quality > 0) {
-          if (item.name !== this.SULFURAS) {
+      if (this.canBeSold(item)) {
+        if (item.name !== this.BRIE && item.name !== this.BACKSTAGE_PASSES) {
+          if (item.quality > 0) {
             item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name === this.BACKSTAGE_PASSES) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name !== this.SULFURAS) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name !== this.BRIE) {
-          if (item.name !== this.BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
-              if (item.name !== this.SULFURAS) {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
           }
         } else {
           if (item.quality < 50) {
             item.quality = item.quality + 1;
+            if (item.name === this.BACKSTAGE_PASSES) {
+              if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1;
+                }
+              }
+              if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1;
+                }
+              }
+            }
+          }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+          if (item.name !== this.BRIE) {
+            if (item.name !== this.BACKSTAGE_PASSES) {
+              if (item.quality > 0) {
+                item.quality = item.quality - 1;
+              }
+            } else {
+              item.quality = item.quality - item.quality;
+            }
+          } else {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
           }
         }
       }
@@ -73,4 +89,4 @@ class Shop {
 module.exports = {
   Item,
   Shop
-}
+};
